8d5d1cba54f1f9feef2d4d748535794182c0dbea
Davide Fucci <dfucci@users.noreply.github.com>
2016-11-28 10:48:14 +0100

    Create CustomLifeException.java

---
+++ src/classes/CustomLifeException.java
@@ -0,0 +1,8 @@
+package classes;
+
+public class CustomLifeException extends Exception {
+	private static final long serialVersionUID = 1L;
+	public CustomLifeException(String message){
+		super(message);
+	}
+}
